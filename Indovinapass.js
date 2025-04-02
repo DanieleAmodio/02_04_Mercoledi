2f4e51c5be4ba7b4323bc89d241c19981d802fd0
DanieleAmodio <amodio_daniele8990@live.it>
2025-04-02 22:16:40 +0200

    prove

--- Indovinapass.js
+++ Indovinapass.js
@@ -1,21 +1,31 @@
 let tentativo = 0
 let passKey = "segreto"
+let div = document.createElement("div")
+let div1 = document.createElement("div1")
+let div3 = document.createElement("div3")
+let pass
+function valore(pass) {
+    do  { let indovina = pass
 
-do  { let indovina = prompt (" Inserisci la pass segreta")
 
-
-if (indovina === passKey) {
-console.log("Puoi entrare!")
-break
-
-} else{
-console.log( "Sbagliato")
-
+        if (indovina === passKey) {
+            div.innerHTML=("Puoi entrare!")
+            document.body.appendChild(div)
+            break
+        
+        } else if (div1){
+            div1.innerHTML=("Sbagliato")
+            document.body.appendChild(div1)
+        }  else 
+            div1.innerHTML +=("Sbagliato")
+            document.body.appendChild(div1)
+        
+         tentativo++
+        
+        
+        } while (tentativo <3)
+        
+        if (tentativo === 3 )
+        div3.innerHTML=(`La pass era ${passKey}, hai finito i tentativi `)
+        document.body.appendChild(div3)
 }
-tentativo++
-
-
-} while (tentativo <3)
-
-if (tentativo === 3 )
-console.log(`La pass era ${passKey}, hai finito i tentativi `)
